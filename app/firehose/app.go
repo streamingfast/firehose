ab19a73e584259fb5a08f39e6324a329ce7795bd
billettc <charles@cbillette.com>
2021-02-08 13:32:37 -0500

    make RealtimeTolerance configurable

--- app/firehose/app.go
+++ app/firehose/app.go
@@ -38,6 +38,7 @@ type Config struct {
 	BlockStreamAddr         string        // gRPC endpoint to get real-time blocks, can be "" in which live streams is disabled
 	GRPCListenAddr          string        // gRPC address where this app will listen to
 	GRPCShutdownGracePeriod time.Duration // The duration we allow for gRPC connections to terminate gracefully prior forcing shutdown
+	RealtimeTolerance       time.Duration
 }
 
 type Modules struct {
@@ -199,7 +200,7 @@ func (a *App) newSubscriptionHub(ctx context.Context, blockStores []dstore.Store
 		fileSourceFactory,
 		liveSourceFactory,
 		hub.Withlogger(a.logger),
-		hub.WithRealtimeTolerance(1*time.Minute),
+		hub.WithRealtimeTolerance(a.config.RealtimeTolerance),
 		hub.WithoutMemoization(), // This should be tweakable on the Hub, by the bstreamv2.Server
 	)
 }
